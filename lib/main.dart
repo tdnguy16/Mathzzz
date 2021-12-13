0ac7e0ebbab2bb50624e386ff88a2e0a50ccd63e
tdnguy16 <tdnguy16@gmail.com>
2021-12-14 01:26:33 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Retrieve Text Input',
+      title: 'Number brakedown',
       home: MyCustomForm(),
     );
   }
 }
 
-// Define a custom Form widget.
 class MyCustomForm extends StatefulWidget {
   const MyCustomForm({Key? key}) : super(key: key);
 
@@ -23,55 +22,162 @@ class MyCustomForm extends StatefulWidget {
   _MyCustomFormState createState() => _MyCustomFormState();
 }
 
-// Define a corresponding State class.
-// This class holds the data related to the Form.
 class _MyCustomFormState extends State<MyCustomForm> {
-  // Create a text controller and use it to retrieve the current value
-  // of the TextField.
+
   final myController = TextEditingController();
 
   @override
   void dispose() {
-    // Clean up the controller when the widget is disposed.
     myController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Retrieve Text Input'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          decoration: const InputDecoration(labelText: "Enter your number"),
-          keyboardType: TextInputType.number,
-          inputFormatters: <TextInputFormatter>[
-            FilteringTextInputFormatter.digitsOnly
-          ],
-          // Only numbers can be entered
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+              ],
+            ),
+            title: const Text('Number brakedown'),
+          ),
+          body: TabBarView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: const InputDecoration(labelText: "Enter your number"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                    ],
+                  controller: myController,
+                  ),
+                ),
+              GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 2,
+                mainAxisSpacing: 2,
+                crossAxisCount: 4,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                    },
+                    child: Text(myController.text.substring(0,1)),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                    },
+                    child: Text(myController.text.substring(1,2)),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                    },
+                    child: Text(myController.text.substring(2,3)),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                    },
+                    child: Text(myController.text.substring(3,4)),
+                  ),
+                  // THOUSAND
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Center(child: Text(myController.text.substring(0,1))),
+                    color: Colors.teal[100],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[100],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[100],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[100],
+                  ),
+                  //HUNDRED
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[200],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Text(myController.text.substring(1,2)),
+                    color: Colors.teal[200],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[200],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[200],
+                  ),
+                  //TEN
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[300],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[300],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Text(myController.text.substring(2,3)),
+                    color: Colors.teal[300],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[300],
+                  ),
+                  //ONE
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[400],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[400],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset('images/0.png'),
+                    color: Colors.teal[400],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    child: Text(myController.text.substring(3,4)),
+                    color: Colors.teal[400],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        // When the user presses the button, show an alert dialog containing
-        // the text that the user has entered into the text field.
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                // Retrieve the text the that user has entered by using the
-                // TextEditingController.
-                content: Text(myController.text),
-              );
-            },
-          );
-        },
-        tooltip: 'Show me the value!',
-        child: const Icon(Icons.text_fields),
-      ),
     );
+
   }
 }
