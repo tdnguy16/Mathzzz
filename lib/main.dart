40d44dcbea26aa46b2da352cd14cd7c40f058cd7
tdnguy16 <tdnguy16@gmail.com>
2021-12-15 15:54:31 +0700

    generate random number

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'random_number.dart';
 
 void main() => runApp(const MyApp());
-  var thousand = '0';
-  var hundred = '0';
-  var ten = '0';
-  var one = '0';
-  var thousandRow = '0';
-  var hundredRow = '0';
-  var tenRow = '0';
-  var oneRow = '0';
 
+void random() {
+  var rng = new Random();
+  for (var i = 0; i < 10; i++) {
+    print(rng.nextInt(100));
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Number brakedown',
+      title: 'Number place value',
       home: MyCustomForm(),
     );
   }
@@ -32,14 +32,48 @@ class MyCustomForm extends StatefulWidget {
 }
 
 class _MyCustomFormState extends State<MyCustomForm> {
+  var x = 0;
+  var randomnumber = '';
+  var thousand = '';
+  var hundred = '';
+  var ten = '';
+  var one = '';
+  var thousand1 = '';
+  var hundred1 = '';
+  var ten1 = '';
+  var one1 = '';
+  var thousand2 = '';
+  var hundred2 = '';
+  var ten2 = '';
+  var one2 = '';
+  var thousand3 = '';
+  var hundred3 = '';
+  var ten3 = '';
+  var one3 = '';
+  var thousand4 = '';
+  var hundred4 = '';
+  var ten4 = '';
+  var one4 = '';
+
+  void _random() {
+    random(min, max){
+      var rn = new Random();
+      return min + rn.nextInt(max - min);
+    }
+    return x = random(1000,9999); // Output : 19, 6, 15..
+  }
+
+  void _reset() {
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(
+        transitionDuration: Duration.zero,
+        pageBuilder: (_, __, ___) => MyApp(),
+      ),
+    );
+  }
 
   final myController = TextEditingController();
-
-  @override
-  void dispose() {
-    myController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,217 +94,226 @@ class _MyCustomFormState extends State<MyCustomForm> {
             children: [
               Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      decoration: const InputDecoration(labelText: "Enter your number"),
-                      keyboardType: TextInputType.number,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.digitsOnly
-                        ],
-                      controller: myController,
-                      ),
-                  ),
                   ElevatedButton(
                       onPressed: () {
+                        _random();
                         setState(() {
-                          thousand = myController.text.substring(0,1);
-                          hundred = myController.text.substring(1,2);
-                          ten = myController.text.substring(2,3);
-                          one = myController.text.substring(3,4);
+                          thousand = x.toString().substring(0,1);
+                          hundred = x.toString().substring(1,2);
+                          ten = x.toString().substring(2,3);
+                          one = x.toString().substring(3,4);
                         });
                       },
-                      child: Text('OK'))
+                      child: Text('Click for a number'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      randomnumber,
+                    ),
+                  ),
                 ],
               ),
-              GridView.count(
-                primary: false,
-                padding: const EdgeInsets.all(20),
-                crossAxisSpacing: 2,
-                mainAxisSpacing: 2,
-                crossAxisCount: 4,
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        thousandRow = thousand;
-                      });
-                    },
-                    child: Text(thousand),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.teal,
-                        textStyle:
-                        const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+              SafeArea(
+                child: GridView.count(
+                  primary: false,
+                  padding: const EdgeInsets.all(20),
+                  crossAxisSpacing: 2,
+                  mainAxisSpacing: 2,
+                  crossAxisCount: 4,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          thousand1 = thousand;
+                          hundred1 = '0';
+                          ten1 = '0';
+                          one1 = '0';
+                        });
+                      },
+                      child: Text(thousand),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.teal,
+                          textStyle:
+                          const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                      ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        hundredRow = hundred;
-                      });
-                    },
-                    child: Text(hundred),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.teal,
-                        textStyle:
-                        const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          hundred2 = hundred;
+                          ten2 = '0';
+                          one2 = '0';
+                        });
+                      },
+                      child: Text(hundred),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.teal,
+                          textStyle:
+                          const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                      ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        tenRow = ten;
-                      });
-                    },
-                    child: Text(ten),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.teal,
-                        textStyle:
-                        const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          ten3 = ten;
+                          one3 = '0';
+                        });
+                      },
+                      child: Text(ten),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.teal,
+                          textStyle:
+                          const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                      ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        oneRow = one;
-                      });
-                    },
-                    child: Text(one),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.teal,
-                        textStyle:
-                        const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          one4 = one;
+                        });
+                      },
+                      child: Text(one),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.teal,
+                          textStyle:
+                          const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                      ),
                     ),
-                  ),
-                  // THOUSAND
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    child: Center(
+                    // THOUSAND
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      child: Center(
+                          child: Text(
+                            thousand1,
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ),
+                      color: Colors.teal[100],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      child: Center(
                         child: Text(
-                          thousandRow,
+                          hundred1,
                           style: TextStyle(fontSize: 30),
                         ),
                       ),
-                    color: Colors.teal[100],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    child: const Center(
-                      child: Text(
-                        '0',
-                        style: TextStyle(fontSize: 30),
-                      ),
+                      color: Colors.teal[100],
                     ),
-                    color: Colors.teal[100],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    child: const Center(
-                      child: Text(
-                        '0',
-                        style: TextStyle(fontSize: 30),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      child: Center(
+                        child: Text(
+                          ten1,
+                          style: TextStyle(fontSize: 30),
+                        ),
                       ),
+                      color: Colors.teal[100],
                     ),
-                    color: Colors.teal[100],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    child: const Center(
-                      child: Text(
-                        '0',
-                        style: TextStyle(fontSize: 30),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      child: Center(
+                        child: Text(
+                          one1,
+                          style: TextStyle(fontSize: 30),
+                        ),
                       ),
+                      color: Colors.teal[100],
                     ),
-                    color: Colors.teal[100],
-                  ),
-                  //HUNDRED
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    color: Colors.teal[200],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    child: Center(
-                      child: Text(
-                        hundredRow,
-                        style: TextStyle(fontSize: 30),
+                    //HUNDRED
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      color: Colors.teal[200],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      child: Center(
+                        child: Text(
+                          hundred2,
+                          style: TextStyle(fontSize: 30),
+                        ),
                       ),
+                      color: Colors.teal[200],
                     ),
-                    color: Colors.teal[200],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    child: const Center(
-                      child: Text(
-                        '0',
-                        style: TextStyle(fontSize: 30),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      child: Center(
+                        child: Text(
+                          ten2,
+                          style: TextStyle(fontSize: 30),
+                        ),
                       ),
+                      color: Colors.teal[200],
                     ),
-                    color: Colors.teal[200],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    child: const Center(
-                      child: Text(
-                        '0',
-                        style: TextStyle(fontSize: 30),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      child: Center(
+                        child: Text(
+                          one2,
+                          style: TextStyle(fontSize: 30),
+                        ),
                       ),
+                      color: Colors.teal[200],
                     ),
-                    color: Colors.teal[200],
-                  ),
-                  //TEN
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    color: Colors.teal[300],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    color: Colors.teal[300],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    child: Center(
-                      child: Text(
-                        tenRow,
-                        style: TextStyle(fontSize: 30),
+                    //TEN
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      color: Colors.teal[300],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      color: Colors.teal[300],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      child: Center(
+                        child: Text(
+                          ten3,
+                          style: TextStyle(fontSize: 30),
+                        ),
                       ),
+                      color: Colors.teal[300],
                     ),
-                    color: Colors.teal[300],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    child: const Center(
-                      child: Text(
-                        '0',
-                        style: TextStyle(fontSize: 30),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      child: Center(
+                        child: Text(
+                          one3,
+                          style: TextStyle(fontSize: 30),
+                        ),
                       ),
+                      color: Colors.teal[300],
                     ),
-                    color: Colors.teal[300],
-                  ),
-                  //ONE
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    color: Colors.teal[400],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    color: Colors.teal[400],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    color: Colors.teal[400],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(2),
-                    child: Center(
-                      child: Text(
-                        oneRow,
-                        style: TextStyle(fontSize: 30),
+                    //ONE
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      color: Colors.teal[400],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      color: Colors.teal[400],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      color: Colors.teal[400],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      child: Center(
+                        child: Text(
+                          one4,
+                          style: TextStyle(fontSize: 30),
+                        ),
                       ),
+                      color: Colors.teal[400],
                     ),
-                    color: Colors.teal[400],
-                  ),
-                ],
+                    FloatingActionButton(
+                      onPressed: _reset,
+                      child: Text('Again'),
+                    )
+                  ],
+                ),
               ),
             ],
           ),
---
+++ lib/random_number.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+var x = 0;
+
+class RandomNumber{
+
+  void _random() {
+    random(min, max){
+      var rn = new Random();
+      return min + rn.nextInt(max - min);
+    }
+    return x = random(1000,9999); // Output : 19, 6, 15..
+  }
+}
+
+RandomNumber new_Random = RandomNumber();
